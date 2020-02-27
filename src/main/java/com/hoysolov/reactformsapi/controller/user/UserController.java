00fe1145e385775172d413c8d483268a0d554c69
Hristiyan Oysolov <47504849+Oysolov@users.noreply.github.com>
2020-02-27 09:37:59 +0200

    Changed the returned string value of gender. (#4)

--- src/main/java/com/hoysolov/reactformsapi/controller/user/UserController.java
+++ src/main/java/com/hoysolov/reactformsapi/controller/user/UserController.java
@@ -7,6 +7,7 @@ import com.hoysolov.reactformsapi.security.UserPrincipal;
 import com.hoysolov.reactformsapi.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class UserController {
                 user.getId(),
                 user.getEmail(),
                 user.getUsername(),
-                user.getGender().name(),
+                StringUtils.capitalize(user.getGender().name().toLowerCase()),
                 user.getDevSkills()
         );
         return userProfile;
